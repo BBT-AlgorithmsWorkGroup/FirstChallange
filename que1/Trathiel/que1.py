4f43ea0decf6d3d7d52a7fd788d847a61b944593
KadirKrykl <kadirkrykl@gmail.com>
2019-11-27 13:16:20 +0300

    q1

--- que1/Trathiel/que1.py
+++ que1/Trathiel/que1.py
@@ -1,1 +1,10 @@
-print("hello world")
+string=input("Enter string:")
+string=string.split(" ")
+race={}
+for item in string:
+    point=0
+    for char in item:
+        point=point+(ord(char.lower()))
+    race[item]=point
+race = sorted(race.items(), key=lambda kv: kv[1])
+print(race.pop()[0])
